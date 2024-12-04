17611ef7d8999c822d656591a622d8f82d93a025
Jake Richards <contact@jakerichards.info>
2024-12-03 20:05:30 -0800

    day 3: fix edge case that wasn't in input
    Old code did not handle a 'don't()' without a 'do()' before end of file

--- rust/day_3/part_2/src/main.rs
+++ rust/day_3/part_2/src/main.rs
@@ -3,16 +3,19 @@ use regex::Regex;
 fn main() {
     let input = fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
 
-    // First lets find 'dont()' and remove everything between it and the next 'do()'
-    let re = Regex::new(r"(?s)don't\(\).*?do\(\)").unwrap();
-
-    let input = re.replace_all(&input, "");
-    println!("{}", input);
-
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut active = true;
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)").unwrap();
     let mut sum = 0;
     for cap in re.captures_iter(&input) {
-        sum += cap[1].parse::<i32>().unwrap() * cap[2].parse::<i32>().unwrap();
+        if &cap[0] == "don't()" {
+            active = false;
+            continue;
+        } else if &cap[0] == "do()" {
+            active = true;
+            continue;
+        } else if active {
+            sum += cap[1].parse::<i32>().unwrap() * cap[2].parse::<i32>().unwrap();
+        }
     }
     println!("Sum: {}", sum);
 }
