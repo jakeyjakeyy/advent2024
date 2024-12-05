b85a3b22d7f2fd752cb14f577dcfddb5585cf5cf
Jake Richards <contact@jakerichards.info>
2024-12-04 22:52:11 -0800

    remove unneeded check and add some comments

--- rust/day_4/part_2/src/main.rs
+++ rust/day_4/part_2/src/main.rs
@@ -4,8 +4,10 @@ fn main() {
     let input = fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
 
     let lines: Vec<&str> = input.lines().collect();
+    // 2D array is a vector ([]) or vectors ([][]) of characters ([0][0] = 'A')
     let mut array_2d: Vec<Vec<char>> = Vec::new();
 
+    // Go through each line, convert it to a vector of characters, and add it to the 2D array
     for line in lines {
         array_2d.push(line.chars().collect());
     }
@@ -17,7 +19,7 @@ fn main() {
             // If we find an 'A', start searching
             if array_2d[i][j] == 'A' {
                 // Make sure we don't break out of array bounds
-                if i - 1 >= 0 && j - 1 >= 0 && i + 1 < array_2d.len() && j + 1 < array_2d[i].len() {
+                if i + 1 < array_2d.len() && j + 1 < array_2d[i].len() {
                     // Check MAS down and to the right (SAM up and to the left)
                     if array_2d[i-1][j-1] == 'M' && array_2d[i+1][j+1] == 'S' {
                         found += 1;
@@ -34,7 +36,6 @@ fn main() {
                     if array_2d[i+1][j-1] == 'S' && array_2d[i-1][j+1] == 'M' {
                         found += 1;
                     }
-
                     if found >=2 {
                         total += 1;
                     }
