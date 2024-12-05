4b6f8ea57833e79e05101873ac7e8b13d9e7059b
Jake Richards <contact@jakerichards.info>
2024-12-05 14:32:03 -0800

    day 5: use vec indexing instead of regex for performance

--- rust/day_5/part_2/src/main.rs
+++ rust/day_5/part_2/src/main.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use regex::Regex;
 
 fn main() {
     let input: String = fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
@@ -11,40 +10,33 @@ fn main() {
     .collect();
     let updates: Vec<&str> = separate[1].split("\n").collect();
 
-
     let mut failures: Vec<&str> = Vec::new();
     let mut counter = 0;
+
     for update in &updates {
+        let split_update = update.split(",").collect::<Vec<&str>>();
         let mut fail = false;
         for rule in &rules {
             if fail {
                 break;
             }
-            let pattern = format!("{}|{}", rule[0], rule[1]);
-            let re = Regex::new(&pattern).unwrap();
             
             let mut rule2_found = false;
-            for cap in re.captures_iter(&update) {
-                if &cap[0] == rule[0] {
+            for val in &split_update {
+                if val == &rule[0] {
                     if rule2_found {
                         fail = true;
                         failures.push(update);
                         break;
                     }
-                } else if &cap[0] == rule[1] {
+                } else if val == &rule[1] {
                     rule2_found = true;
                 }
             }
         }
-        // if !fail {
-        //     let vals = update.split(",").collect::<Vec<&str>>();
-        //     let mid = vals.len() / 2;
-        //     let val = vals[mid].parse::<i32>().unwrap();
-        //     sum += val;
-        // }
         counter += 1;
         println!("{} of {}", counter, updates.len()); 
-    }
+        }
 
     println!("Failures: {}", failures.len());
     let mut sum = 0;
