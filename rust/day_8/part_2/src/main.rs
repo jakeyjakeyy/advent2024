f7f86664f18ec5a0f17fbe0cdae9bca2b3977c3f
Jake Richards <contact@jakerichards.info>
2024-12-08 17:24:07 -0800

    part_2, fix antinode collection; from vec to hashmap so theres now only one key per letter

--- rust/day_8/part_2/src/main.rs
+++ rust/day_8/part_2/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 fn main() {
     let input = fs::read_to_string("src/input.sample.txt").unwrap();
     let lines = input.lines();
@@ -19,7 +19,8 @@ fn main() {
         }
     }
 
-    let mut antinodes: Vec<(char, Vec<(i32, i32)>)> = Vec::new();
+    let mut antinodes: HashMap<char, Vec<(i32, i32)>> = HashMap::new();
+    let mut seen_tuples: HashSet<(i32, i32)> = HashSet::new();
     for (ch, coords) in coord_map.iter() {
         for (x,y) in coords {
             for (x2, y2) in coords.iter() {
@@ -35,16 +36,16 @@ fn main() {
                 let antinode_2 = (x2 + dx2, y2 + dy2);
 
                 if antinode_1.0 >= 0 && antinode_1.0 < arr_2d[0].len() as i32 &&
-                    antinode_1.1 >= 0 && antinode_1.1 < arr_2d.len() as i32 {
-                    if !antinodes.iter().any(|(ch, coords)| coords.contains(&antinode_1)) {
-                        antinodes.push((*ch, vec![antinode_1]));
-                    }
+                    antinode_1.1 >= 0 && antinode_1.1 < arr_2d.len() as i32 &&
+                    !seen_tuples.contains(&antinode_1) {
+                        antinodes.entry(*ch).or_insert(Vec::new()).push(antinode_1);
+                        seen_tuples.insert(antinode_1);
                 }
                 if antinode_2.0 >= 0 && antinode_2.0 < arr_2d[0].len() as i32 &&
-                    antinode_2.1 >= 0 && antinode_2.1 < arr_2d.len() as i32 {
-                    if !antinodes.iter().any(|(ch, coords)| coords.contains(&antinode_2)) {
-                        antinodes.push((*ch, vec![antinode_2]));
-                    }
+                    antinode_2.1 >= 0 && antinode_2.1 < arr_2d.len() as i32 &&
+                    !seen_tuples.contains(&antinode_2) {
+                        antinodes.entry(*ch).or_insert(Vec::new()).push(antinode_2);
+                        seen_tuples.insert(antinode_2);
                 }
             }
         }
@@ -52,6 +53,7 @@ fn main() {
 
     let mut total = 0;
     for (ch, coords) in antinodes {
+        println!("{}: {:?}", ch, coords);
         total += coords.len();
     }
     println!("{}", total);
