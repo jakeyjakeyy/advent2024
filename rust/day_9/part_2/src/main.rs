988c5b4f8a385798a61828164a36620f77ba8963
Jake Richards <contact@jakerichards.info>
2024-12-09 21:49:56 -0800

    quick little advent day

--- rust/day_9/part_2/src/main.rs
+++ rust/day_9/part_2/src/main.rs
@@ -1,64 +1,14 @@
 use std::fs;
-use std::collections::HashMap;
 fn main() {
-    let input = fs::read_to_string("src/input.sample.txt").unwrap();
+    let input = fs::read_to_string("src/input.txt").unwrap();
+    let parsed_input: Vec<String>;
+
+    parsed_input = parse_input(&input);
+
+    let reordered_input = reorder_input(parsed_input);
     
-    let mut files: Vec<i32> = Vec::new();
-    let mut empty_spaces: Vec<i32> = Vec::new();
-    let mut i = 0;
-    for c in input.chars() {
-        if i % 2 == 0 {
-            files.push(c.to_digit(10).unwrap() as i32);
-        } else {
-            empty_spaces.push(c.to_digit(10).unwrap() as i32);
-        }
-        i += 1;
-    }
-    // println!("Files: {:?}", files);
-    // println!("Empty spaces: {:?}", empty_spaces);
-
-    let mut ordered_files: Vec<i32> = files.clone();
-    let mut ordered_ids: Vec<i32> = (0..ordered_files.len() as i32).collect();
-    files.reverse();
-
-    let mut id = files.len() - 1;
-    let mut offset = 0;
-    for file in files {
-        if id == 0 {
-            break;
-        }
-        for i in 0..empty_spaces.len() {
-            if empty_spaces[i] >= file &&
-            i <= id {
-                // println!("Empty spaces: {:?}", empty_spaces);
-                println!("File: {} of size {} fitting into slot {} of size {}", id, file, i, empty_spaces[i]);
-                let original_space = empty_spaces[i];
-                if id >= empty_spaces.len() {
-                    empty_spaces[id - 1] = file;
-                    let original_file = ordered_files.pop();
-                    ordered_files.insert(i , original_file.unwrap());
-                } else {
-                    // println!("removing empty space at index {}", id + 1 + offset);
-                    // println!("turning empty space at index {} into {}", id + offset, empty_spaces[id + offset] + empty_spaces[id + 1 + offset] + file);
-                    empty_spaces[id + offset] = empty_spaces[id + offset] + empty_spaces[id +  1 + offset] + file;
-                    empty_spaces.remove(id + offset + 1);
-                    offset = 1;
-                    let original_file = ordered_files[id];
-                    ordered_files.remove(id);
-                    ordered_files.insert(i, original_file);
-                }
-                empty_spaces[i] = 0;
-                empty_spaces.insert(i + 1, original_space - file);
-                println!("Ordered files: {:?}", ordered_files);
-                break;
-            }
-        }
-        id -= 1;
-    }
-
-    println!("Ordered files: {:?}", ordered_files);
-    println!("Empty spaces: {:?}", empty_spaces);
-
+    let checksum = calculate_checksum(reordered_input);
+    println!("Checksum: {}", checksum);
 }
 
 fn parse_input(input: &str) -> Vec<String> {
@@ -84,35 +34,78 @@ fn parse_input(input: &str) -> Vec<String> {
 }
 
 fn reorder_input(input: Vec<String>) -> Vec<String> {
-    println!("Input: {:?}", input);
-    let mut reordered_input: Vec<String> = Vec::new();
+    let mut reordered_input: Vec<String> = input.clone();
 
     let mut replacement_index = input.len() - 1;
-    while replacement_index > 0 {
-        for i in 0..input.len() {
-            if replacement_index < i {
-                break;
+    let mut id: u32 = input[replacement_index].parse::<u32>().unwrap();
+    let mut i = 0;
+    while i < input.len() {
+        if replacement_index == 0 {
+            break;
+        }
+        if i == reordered_input.len() - 1 &&
+        replacement_index != 0 ||
+        i >= replacement_index {
+                replacement_index -= calculate_file_size(&reordered_input, replacement_index) as usize;
+                i = 0;
+                loop {
+                    if input[replacement_index] == "." {
+                        replacement_index -= 1;
+                    } else {
+                        break;
+                    }
+                }
+                id = input[replacement_index].parse::<u32>().unwrap();
+        }
+        if reordered_input[i] != "." {
+            i += 1;
+            continue;
+        }
+ 
+        let free_space = calculate_free_space(&reordered_input, i);
+        let file_size = calculate_file_size(&reordered_input, replacement_index);
+        if free_space >= file_size {
+            for j in 0..file_size {
+                reordered_input[replacement_index - j as usize] = ".".to_string();
+                reordered_input[i + j as usize] = id.to_string();
             }
-            if input[i] != "." {
-                reordered_input.push(input[i].clone());
-                continue;
+            replacement_index -= file_size as usize;
+            loop {
+                if input[replacement_index] == "." {
+                    replacement_index -= 1;
+                } else {
+                    break;
+                }
             }
-            while input[replacement_index] == "." && replacement_index > i {
-                replacement_index -= 1;
-            }
-            let freesize = calculate_size(input.clone(), i, false);
-            println!("Freesize: {}", freesize);
-            let filesize = calculate_size(input.clone(), replacement_index, true);
-            println!("Filesize: {}", filesize);
-            if freesize < filesize {
-                reordered_input.push(input[i].clone());
-                continue;
-            }
-            reordered_input.push(input[replacement_index].clone());
-            replacement_index -= filesize as usize;
+
+            id = input[replacement_index].parse::<u32>().unwrap();
+            i = 0;
+        } else {
+            i += 1;
         }
     }
     reordered_input
+}
+
+fn calculate_free_space(input: &Vec<String>, index: usize) -> u32 {
+    let mut free_space = 0;
+    let mut i = index;
+    while i < input.len() && input[i] == "." {
+        free_space += 1;
+        i += 1;
+    }
+    free_space
+}
+
+fn calculate_file_size(input: &Vec<String>, index: usize) -> u32 {
+    let mut file_size = 0;
+    let case = &input[index];
+    let mut i = index;
+    while i > 0 && input[i] == *case {
+        file_size += 1;
+        i -= 1;
+    }
+    file_size
 }
 
 fn calculate_checksum(input: Vec<String>) -> u64 {
@@ -126,24 +119,4 @@ fn calculate_checksum(input: Vec<String>) -> u64 {
         checksum += input[i].parse::<u64>().unwrap() * i as u64;
     }
     checksum
-}
-
-fn calculate_size(input: Vec<String>, replacement_index: usize, get_filesize: bool) -> u64 {
-
-    let mut index = replacement_index;
-    let mut filesize: u64 = 0;
-    let file_content = input[index].clone();
-    loop {
-        if input[index] == file_content {
-            filesize += 1;
-        } else {
-            break;
-        }
-        if get_filesize {
-            index -= 1;
-        } else {
-            index += 1;
-        }
-    }
-    return filesize;
 }
